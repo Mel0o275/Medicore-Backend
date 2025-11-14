8c4ba5fa6183f1f85cf3900d0b2d8d516a8db6b6
youssefsalehs <youssef.ashraf.saleh@gmail.com>
2025-11-14 21:47:15 +0200

    update: trying to fix a problem with front

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -7,7 +7,7 @@ class APIFeatures {
   }
   filter() {
     const queryObj = { ...this.queryString };
-    const excludedFields = ["page", "sort", "limit", "fields", "search"];
+    const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
