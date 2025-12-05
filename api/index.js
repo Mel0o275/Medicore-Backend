221ca7b92aaf762c497601b175a911b9c10a44f4
Melo <158046523+Mel0o275@users.noreply.github.com>
2025-12-05 14:02:42 +0200

    linkkk

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ const URL = process.env.DB_URL;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://medicore-backend.vercel.app/api/v1",
+  "https://medicore-theta.vercel.app/"
 ];
 
 const cors = require("cors");
