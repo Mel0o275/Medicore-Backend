f7c1ef9f6d62e10d437cfee99f4a777a966709e0
Rahma Magdy <rahma.magdy1411@gmail.com>
2025-12-05 17:04:38 +0200

    last update

--- api/index.js
+++ api/index.js
@@ -7,6 +7,12 @@ const app = express();
 const port = process.env.PORT || 4000;
 const URL = process.env.DB_URL;
 
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://medicore-backend.vercel.app/api/v1",
+//   "https://medicore-theta.vercel.app/",
+// ];
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://medicore-backend.vercel.app",
